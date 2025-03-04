7b78b177683c7329c0202619e09220a0dbf7ded2
#ox0d <muhammed.asaad75@gmail.com>
2025-03-04 20:49:19 +0300

    Adjusted margin in welcome section for improved layout consistency

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <section class="pt-10">
             <x-section-heading>Featured Jobs</x-section-heading>
     
-            <div class="grid lg:grid-cols-3 gap-8 mt-6">
+            <div class="grid lg:grid-cols-3 gap-8 mt-5">
                 <x-job-card />
                 <x-job-card />
                 <x-job-card />
