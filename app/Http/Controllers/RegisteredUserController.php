b093e475611d10036f7d81034a3f50c3f761f621
#ox0d <muhammed.asaad75@gmail.com>
2025-03-07 12:25:39 +0300

    Added user registration and login functionality; created RegisteredUserController and SessionController, updated routes, and added registration form view

---
+++ app/Http/Controllers/RegisteredUserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\File;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $userAttributes = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
+            'password' => ['required', 'min:8', 'confirmed'],
+        ]);
+
+        $employerAttributes = $request->validate([
+            'employer' => ['required', 'string', 'max:255'],
+            'logo' => ['required', File::types(['jpeg', 'png', 'webp'])],
+        ]);
+
+        $user = User::create($userAttributes);
+
+        $logoPath = $request->logo->store('logos');
+
+        $user->employer()->create([
+            'name' => $employerAttributes['employer'],
+            'logo' => $logoPath,
+        ]);
+
+        Auth::login($user);
+
+        return redirect()->route('jobs.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,17 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class, 'index'])->name('jobs.index');
+
+Route::prefix('/')->group(function () {
+    Route::get('register', [RegisteredUserController::class, 'create'])->name('register.create');
+    Route::post('register', [RegisteredUserController::class, 'store'])->name('register.store');
+
+    Route::get('login', [SessionController::class, 'create'])->name('login.create');
+    Route::post('login', [SessionController::class, 'store'])->name('login.store');
+    Route::delete('logout', [SessionController::class, 'destroy'])->name('logout.destroy');
+});
